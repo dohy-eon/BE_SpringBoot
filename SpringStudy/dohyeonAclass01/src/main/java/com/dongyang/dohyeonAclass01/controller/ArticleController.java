60d9b9dca547a40201fabb90bdbe1c3f99c5ab49
HYEON <titeotty12@gmail.com>
2025-04-14 11:23:36 +0900

    h2 console 추가

--- SpringStudy/dohyeonAclass01/src/main/java/com/dongyang/dohyeonAclass01/controller/ArticleController.java
+++ SpringStudy/dohyeonAclass01/src/main/java/com/dongyang/dohyeonAclass01/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.dongyang.dohyeonAclass01.controller;
 import com.dongyang.dohyeonAclass01.dto.ArticleForm;
 import com.dongyang.dohyeonAclass01.entity.Article;
 import com.dongyang.dohyeonAclass01.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ArticleController {
 
+    @Autowired
     ArticleRepository articleRepository;
-    
+
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "boards/new";
