e10eb435ae631f04f667f5cdbe4ffe28f9e26909
HYEON <titeotty12@gmail.com>
2025-05-12 11:47:47 +0900

    Hello REST 생성

--- SpringStudy/dohyeonAclass01/src/main/java/com/dongyang/dohyeonAclass01/dto/ArticleForm.java
+++ SpringStudy/dohyeonAclass01/src/main/java/com/dongyang/dohyeonAclass01/dto/ArticleForm.java
@@ -1,44 +1,17 @@
 package com.dongyang.dohyeonAclass01.dto;
 
 import com.dongyang.dohyeonAclass01.entity.Article;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@Getter
+@Setter
 public class ArticleForm {
     private Long id;
     private String title;
     private String content;
-
-    public Article ArticleForm() {
-        return new Article(null, title, content);
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" + "title=" + title + ", content=" + content + '}';
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public Article toEntity() {
         return new Article(id, title, content);
